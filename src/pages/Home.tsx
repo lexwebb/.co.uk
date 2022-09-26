3fd0375fb9b1065061a81f3ef708c93391c3b18d
Lex Southin-Webb <lexwebb@gmail.com>
2022-09-27 00:07:50 +0100

    Start of home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,22 @@
 import React from "react";
+import styles from "./Home.module.scss";
+import me from "../images/me.png";
+import GithubIcon from "../icons/GithubIcon";
 
 const Home = () => {
   return (
-    <div>
-      <span>Coming Soon</span>
+    <div className={styles.container}>
+      <div className={styles.image}>
+        <img src={me} alt="Lex's Face" />
+      </div>
+      <div className={styles.links}>
+        <a href="https://github.com/lexwebb">
+          <GithubIcon />
+          Github // lexwebb
+        </a>
+      </div>
+      {/* https://github.com/remarkjs/react-markdown */}
+      <div className={styles.about}>content here</div>
     </div>
   );
 };
